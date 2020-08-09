3b5ef397f7f1bdae250ec523589781887bbd0ef7
Gamaliel Troncoso <gamaliel.troncoso@gmail.com>
2020-08-09 18:33:22 -0400

    update

--- src/seia/BackgroundController.java
+++ src/seia/BackgroundController.java
@@ -599,8 +599,8 @@ public class BackgroundController implements Initializable {
         else{
             drawPane.setDisable(true);
         }
-        if (!selectButton.getStyle().equals("-fx-background-color: #FFFFFF")) {
-            selectButton.setStyle("-fx-background-color: #FFFFFF");
+        if (!selectButton.getStyle().equals("-fx-background-color: #ccd0e6")) {
+            selectButton.setStyle("-fx-background-color: #ccd0e6");
         }  
     }     
     
@@ -614,8 +614,8 @@ public class BackgroundController implements Initializable {
         else{
             contenidoPDF.setDisable(true);
         }
-        if (!drawButton.getStyle().equals("-fx-background-color: #FFFFFF")) {
-            drawButton.setStyle("-fx-background-color: #FFFFFF");
+        if (!drawButton.getStyle().equals("-fx-background-color: #ccd0e6")) {
+            drawButton.setStyle("-fx-background-color: #ccd0e6");
         }
     }   
     
